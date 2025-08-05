3066f65f6afcc74496198a31ec8eb7b337446333
Rony Boban <ronyinc@hotmail.com>
2025-08-05 15:30:12 -0700

    Update the DAG logic for the customer_etl code

--- dags/customer_etl_dag_version2.py
+++ dags/customer_etl_dag_version2.py
@@ -9,15 +9,15 @@ default_args = {
 }
 
 with DAG(
-    dag_id ='customer_etl_dag', 
+    dag_id ='customer_etl_dag_version2', 
     default_args=default_args,
-    start_date=datetime(2025, 7, 5),
-    schedule_interval='@daily',
-    catchup=True
+    start_date=datetime(2025, 7, 8),
+    schedule=None,
+    catchup=False
 ) as dag:
     
     run_etl = BashOperator(
         task_id='run_customer_loyalty_etl',
-        bash_command='bash /opt/spark-apps/customer_etl/shell/customer_etl_job_airflow_version_2.sh {{ ds }}'
+        bash_command='bash /opt/spark-apps/customer_etl/shell/customer_etl_job_airflow_version_3.sh {{ ds }}'
     )
     run_etl
